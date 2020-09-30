e1550c7c3383fa2e46c5893445dbac706ec6bd7e
Bebisweta <63045635+Bebisweta@users.noreply.github.com>
2020-09-30 21:51:46 +0530

    Add files via upload

---
+++ src/main/java/com/nt/CostCalculateApplication.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CostCalculateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CostCalculateApplication.class, args);
+	}
+
+}
